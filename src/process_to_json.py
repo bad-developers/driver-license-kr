ad599424866cf166b34466918f9527524b1cc4d4
Thai Phan <thaiphan2712@gmail.com>
2023-04-24 14:22:59 +0900

    update source code

--- src/process_to_json.py
+++ src/process_to_json.py
@@ -3,40 +3,41 @@ import pymongo
 import re
 
 if __name__ == "__main__":
-    my_client = pymongo.MongoClient("mongodb://localhost:27017/")
+    # my_client = pymongo.MongoClient("mongodb://localhost:27017/")
+    my_client = pymongo.MongoClient("mongodb://root:example@localhost:27017/")
     my_db = my_client["driver"]
-    my_col = my_db["test"]
+    my_col = my_db["question"]
     question_head = "QQQ"
     answer_head = "QAA"
     correct_answer = "AAA"
     end_line_head = "%%%"
     my_dict = {}
-
+    count = 1
     with open("../question_short.txt", "r", encoding="utf8") as file:
-        for line in file:
-            if "\n" in line:
-                line = line.replace('\n', '')
+        for cur_line in file:
+            if "\n" in cur_line:
+                cur_line = cur_line.replace('\n', '')
 
-            if question_head in line:
+            if question_head in cur_line:
+                my_dict["question_id"] = count
                 my_dict["question"] = {
-                    "id": 1,
-                    "text": line.replace(question_head, ''),
+                    "id": count,
+                    "text": cur_line.replace(question_head, ''),
                     "type": "Sentence type 1 answer",
                     "media": ""
                 }
-            elif answer_head in line:
+            elif answer_head in cur_line:
                 if "answerList" not in my_dict:
                     my_dict["answerList"] = []
 
                 my_dict["answerList"].append({
-                    "id": len(my_dict["answerList"]),
-                    "text": line.replace(answer_head, '')
+                    "id": len(my_dict["answerList"]) + 1,
+                    "text": cur_line.replace(answer_head, '')
                 })
-            elif 'AAA' in line:
-                # x = mycol.insert_one(mydict)
-                my_dict["correctAnswer"] = line.replace(correct_answer, '')
-            elif "%%%" in line:
-                # x = mycol.insert_one(mydict)
-                # mydict = {}
-
-                print("aaaa")
+            elif correct_answer in cur_line:
+                my_dict["correctAnswer"] = cur_line.replace(correct_answer, '')
+            elif end_line_head in cur_line:
+                x = my_col.insert_one(my_dict.copy())
+                print(count)
+                count = count + 1
+                my_dict = {}
