6df03a2214742a7f4ba11d4033a64b8b20b303dc
Thai Phan <thaiphan2712@gmail.com>
2023-04-25 12:07:10 +0900

    update source code

--- src/process_to_json.py
+++ src/process_to_json.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     my_col = my_db["question"]
     question_head = "QQQ"
     answer_head = "QAA"
+    description_head = "DDD"
     correct_answer = "AAA"
     end_line_head = "%%%"
     my_dict = {}
@@ -19,23 +20,47 @@ if __name__ == "__main__":
                 cur_line = cur_line.replace('\n', '')
 
             if question_head in cur_line:
-                my_dict["question_id"] = count
-                my_dict["question"] = {
-                    "id": count,
-                    "text": cur_line.replace(question_head, ''),
-                    "type": "Sentence type 1 answer",
-                    "media": ""
-                }
-            elif answer_head in cur_line:
-                if "answerList" not in my_dict:
-                    my_dict["answerList"] = []
 
-                my_dict["answerList"].append({
-                    "id": len(my_dict["answerList"]) + 1,
-                    "text": cur_line.replace(answer_head, '')
+                cur_line = cur_line.replace(question_head, '')
+                question_id = re.search(r'^\d+', cur_line).group()
+                my_dict["question_id"] = question_id
+                my_dict["question_type"] = {
+                    "type": "Text Only",
+                    "detail": "Question type 1 answer",
+                    "url": ""
+                }
+                my_dict["question"] = {
+                    "text": cur_line,
+                }
+
+            elif answer_head in cur_line:
+                cur_line = cur_line.replace(answer_head, '')
+                if "answer_list" not in my_dict:
+                    my_dict["answer_list"] = []
+
+                my_dict["answer_list"].append({
+                    "id": len(my_dict["answer_list"]) + 1,
+                    "text": cur_line
                 })
+
+            elif description_head in cur_line:
+                cur_line = cur_line.replace(description_head, '')
+                if "description_list" not in my_dict:
+                    my_dict["description_list"] = []
+                my_dict["description_list"].append({
+                    "id": len(my_dict["description_list"]) + 1,
+                    "text": cur_line
+                })
+                my_dict["question_type"] = {
+                    "type": "With Media",
+                    "detail": "",
+                    "url": ""
+                }
+
             elif correct_answer in cur_line:
-                my_dict["correctAnswer"] = cur_line.replace(correct_answer, '')
+                cur_line = cur_line.replace(correct_answer, '')
+                my_dict["correct_answer"] = cur_line
+
             elif end_line_head in cur_line:
                 x = my_col.insert_one(my_dict.copy())
                 print(count)
